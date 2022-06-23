36212ed07ef004c8b2aebac0a4e1f5adb42c13d9
mjkl-gh <9350991+mjkl-gh@users.noreply.github.com>
2022-06-23 22:01:58 +0200

    Remove led example

--- interface/src/project/DemoProject.tsx
+++ interface/src/project/DemoProject.tsx
@@ -7,9 +7,6 @@ import { Tab } from '@mui/material';
 import { RouterTabs, useRouterTab, useLayoutTitle } from '../components';
 
 import DemoInformation from './DemoInformation';
-import LightStateRestForm from './LightStateRestForm';
-import LightMqttSettingsForm from './LightMqttSettingsForm';
-import LightStateWebSocketForm from './LightStateWebSocketForm';
 
 const DemoProject: FC = () => {
   useLayoutTitle("Demo Project");
@@ -19,15 +16,9 @@ const DemoProject: FC = () => {
     <>
       <RouterTabs value={routerTab}>
         <Tab value="information" label="Information" />
-        <Tab value="rest" label="REST Example" />
-        <Tab value="socket" label="WebSocket Example" />
-        <Tab value="mqtt" label="MQTT Settings" />
       </RouterTabs>
       <Routes>
         <Route path="information" element={<DemoInformation />} />
-        <Route path="rest" element={<LightStateRestForm />} />
-        <Route path="mqtt" element={<LightMqttSettingsForm />} />
-        <Route path="socket" element={<LightStateWebSocketForm />} />
         <Route path="/*" element={<Navigate replace to="information" />} />
       </Routes>
     </>
